4e8eb5bdbc4844a997b713b290e1128023d7ade0
joshdotjs <joshdotjs@gmail.com>
2023-04-27 10:29:05 -0500

    html structure

--- vanilla-js/main.js
+++ vanilla-js/main.js
@@ -1,1 +1,8 @@
 console.log('hello world');
+
+const qs = (selector) => document.querySelector(selector);
+const qsa = (selector) => document.querySelectorAll(selector);
+
+
+const rows = qs('.row');
+console.log('rows: ', rows);
