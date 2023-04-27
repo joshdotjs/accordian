7b58ad98bf872613b76fec1b08b6a62ca4c7a63b
joshdotjs <joshdotjs@gmail.com>
2023-04-27 10:48:36 -0500

    animate accordian

--- vanilla-js/main.js
+++ vanilla-js/main.js
@@ -1,19 +1,60 @@
-console.log('hello world');
-
 const qs = (selector) => document.querySelector(selector);
 const qsa = (selector) => document.querySelectorAll(selector);
 
+// ==============================================
 
 const rows = qsa('.row');
 console.log('rows: ', rows);
 
-rows.forEach((row, idx) => {
+// ==============================================
+
+
+let prev_idx;
+let tl_prev;
+
+// ==============================================
+
+rows.forEach((row, i) => {
+  
+  const a = row.querySelector('.a');
+  const arrow = row.querySelector('.arrow');
+  const height = row.offsetHeight + a.offsetHeight + 16; // + 1rem margin-bottom
+  console.log('a: ', a);
+  console.log('row: ', row);
+  console.log('arrow: ', arrow);
+  console.log('height: ', height);
+  
   row.addEventListener('click', (e) => {
 
-    console.log('clicked row: ', idx);
+    console.log('clicked row: ', i);
 
-    gsap.to(row, {
-      x: '100px'
+    const tl = gsap.timeline();
+    const duration = 0.33;
+
+    tl.to(arrow, {
+      rotate: "180deg",
+      onStart: () => {
+        // disableClicks();
+      },
+      duration
     });
+
+    tl.to(
+      row,
+      {
+        height: `${height}px`,
+        color: "#40bf57",
+        onComplete: () => {
+          // tl_prev.current = tl;
+          // prev_idx.current = idx;
+          // enableClicks();
+        },
+        duration
+      },
+      "<="
+    );
+
   });
 });
+
+// ==============================================
